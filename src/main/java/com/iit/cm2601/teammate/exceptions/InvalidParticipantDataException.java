94c84f6743d67e7e66e481ee922e47616ad76356
Hasindu Sankalpa <ubayawickrama7@gmail.com>
2025-11-24 02:26:03 +0530

    Added two custom exceptions - ParticipantFileException - InvalidParticipantDataException

---
+++ src/main/java/com/iit/cm2601/teammate/exceptions/InvalidParticipantDataException.java
@@ -0,0 +1,7 @@
+package com.iit.cm2601.teammate.exceptions;
+
+public class InvalidParticipantDataException extends Exception {
+    public InvalidParticipantDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/iit/cm2601/teammate/exceptions/ParticipantFileException.java
@@ -0,0 +1,12 @@
+package com.iit.cm2601.teammate.exceptions;
+
+public class ParticipantFileException extends Exception {
+    public ParticipantFileException(String message) {
+        super(message);
+    }
+
+    public ParticipantFileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
